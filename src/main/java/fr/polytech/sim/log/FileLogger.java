5bc9931aee7a2664cfb509ed53c18c83f94bffa4
Axel Milioni <Axel.milioni@gmail.com>
2024-04-12 10:31:33 +0200

    exercice 5

--- src/main/java/fr/polytech/sim/log/FileLogger.java
+++ src/main/java/fr/polytech/sim/log/FileLogger.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 /**
  * File logger.
  */
-public class FileLogger extends NamedLogger {
+class FileLogger extends NamedLogger {
     private static final String FILE_NAME = "logs.txt";
 
     /**
@@ -19,9 +19,7 @@ public class FileLogger extends NamedLogger {
     }
 
     @Override
-    synchronized public void log(String format, Object... args) {
-        String entry = String.format(format, args);
-        String message = String.format("%s\t%s\n", this.name, entry);
+    public void logMethod(String message) {
         try (FileWriter fileWriter = new FileWriter(FILE_NAME, true)) {
             fileWriter.write(message);
         } catch (IOException e) {
